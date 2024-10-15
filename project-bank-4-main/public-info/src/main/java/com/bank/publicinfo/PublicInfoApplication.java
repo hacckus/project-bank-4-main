5cec21fd7a0b779e8a0938258d8a8e3aabc11d3f
Ibragim Abbasov <lic-ibragim@mail.ru>
2024-10-15 15:57:28 +0300

    task1

--- project-bank-4-main/public-info/src/main/java/com/bank/publicinfo/PublicInfoApplication.java
+++ project-bank-4-main/public-info/src/main/java/com/bank/publicinfo/PublicInfoApplication.java
@@ -2,10 +2,10 @@ package com.bank.publicinfo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 
-@EnableEurekaClient
 @SpringBootApplication
+@ComponentScan(basePackages = {"ru.example.publicinfo", "ru.example.common"})
 public class PublicInfoApplication {
     public static void main(String[] args) {
         SpringApplication.run(PublicInfoApplication.class, args);
